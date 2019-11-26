0f41b1f16b37e999077d8523fd9c8dab1e6fcb20
Bertuleza <58227161+Bertuleza@users.noreply.github.com>
2019-11-26 15:40:21 -0300

    adicionei arquivos via upload

---
+++ hello githhub/src/laisbertuleza/hellogith.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package laisbertuleza;
+
+/**
+ * @author lais_
+ *  exemplo de interação
+ */
+public class hellogith {
+
+	
+	public static void main(String[] args) {
+		System.out.println("hello git");
+
+	}
+
+}
